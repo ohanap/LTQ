9b334d97840dc2c6527ecc8a5dc6c4b4a8415535
lippePhi <ohanap@yahoo.com>
2013-12-21 16:34:54 -0500

    New website

--- Eclipse-workspace/matchtoqualitysite1/src/com/matchtoquality/MailServlet.java
+++ Eclipse-workspace/matchtoqualitysite1/src/com/matchtoquality/MailServlet.java
@@ -3,6 +3,7 @@ package com.matchtoquality;
 import javax.mail.Transport;
 
 
+
 import java.util.Properties; 
 import javax.mail.Message; 
 import javax.mail.Session; 
@@ -20,21 +21,20 @@ public class MailServlet extends HttpServlet{
   
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {         
 
-		String email = req.getParameter("textfield2");
-		String comments = req.getParameter("textfield22");
-		String phone = req.getParameter("textfield3");
-		String name = req.getParameter("textfield");
-		
-		String SMTP_AUTH_USER ="devtoquality@gmail.com";
+   		String email = req.getParameter("parEmail");
+   		String comments = req.getParameter("theMessage");
+   		String name = req.getParameter("firstName") + " " + req.getParameter("lastName");    
+
+		String SMTP_AUTH_USER ="sales@matchtoquality.com";
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props, null);
 		try      {          Message msg = new MimeMessage(session);                     
 			                msg.setFrom(new InternetAddress(SMTP_AUTH_USER, "MatchToQuality"));          
 			                msg.addRecipient(Message.RecipientType.TO,new InternetAddress(SMTP_AUTH_USER));          
-			                msg.setSubject(name + email);                     
-			                msg.setContent(phone + comments, "text/html");         
+			                msg.setSubject(name +" "+ email);                     
+			                msg.setContent(comments, "text/html");         
 			                Transport.send(msg);
-			                resp.sendRedirect("/Matchtoquality.jsp");
+			                resp.sendRedirect("/contacts.jsp");
 			     }      
 		catch (Exception e)      {         
 			
